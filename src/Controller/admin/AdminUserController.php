a51dc34b63506a663bcf8f234453491cfbab5d16
Araya RAKSACHOB <araya.raksachob@lapiscine.pro>
2025-05-12 17:18:11 +0200

    SaveNewUser

--- src/Controller/admin/AdminUserController.php
+++ src/Controller/admin/AdminUserController.php
@@ -3,29 +3,62 @@
 namespace App\Controller\admin;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class AdminUserController extends AbstractController
 {
     #[Route('/admin/create-user', name: 'admin-create-user')]
-    public function CreateAdmin(Request $request, UserPasswordHasherInterface $passwordHasher){
+    public function CreateAdmin(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response{
 
+        //si le formulaire a été soumis en POST
         if ($request->isMethod("POST")) {
            
+            //récupération des données du formulaire HTML
             $password = $request->request->get('password');
             $email = $request->request->get('email');
 
+            //création d'un nouvel utilisateur
             $user = new User();
-            $passwordHashed = $passwordHasher->hashPassword($user, $password);
+            //on set le mot de passe
+            $user->setEmail($email);
+            
 
-            dump($passwordHashed);
-            dump($email);die;
+            //on hash le mot de passe
+            $passwordHashed = $passwordHasher->hashPassword($user, $password);
+            //Enregistre le mdp sécurisé
+            $user->setPassword($passwordHashed);
+
+            //on set le role
+            $user->setRoles(['ROLE_ADMIN']);
+
+            //enregistrement de l'utilisateur dans la BDD
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Nouvel administrateur créé avec succès !');
+
+            //redirection vers la page de création d'utilisateur
+            return $this->redirectToRoute('admin-create-user');
+
+            // 2eme méthode:
+            // public function createAdmin($email, $passwordHashed) {
+            //     $this->email = $email;
+            //     $this->password = $passwordHashed;
+            //     $this->roles = ['ROLE_ADMIN'];
+            // }
+            // $user->createAdmin($email, $passwordHashed);
+
+
 
         }
+        //si le formulaire n'a pas été soumis, on affiche le formulaire
         return $this->render('/admin/user/create-user.html.twig');
     }
 }
